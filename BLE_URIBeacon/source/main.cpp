8f4dc334d6f3105147bf9bb7c40524b31cd9e2af
Rohit Grover <rohit.grover@arm.com>
2015-10-13 11:45:02 +0100

    update signature of the disconnection callback; necessary due to change in BLE_API

--- BLE_URIBeacon/source/main.cpp
+++ BLE_URIBeacon/source/main.cpp
@@ -53,7 +53,7 @@ void timeout(void)
 /**
  * Callback triggered upon a disconnection event. Needs to re-enable advertisements.
  */
-void disconnectionCallback(Gap::Handle_t handle, Gap::DisconnectionReason_t reason)
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *)
 {
     ble.startAdvertising();
 }

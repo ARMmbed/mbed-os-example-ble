95532c625905fb0a44b34f5638c29fe517b0e0d6
paul-szczepanek-arm <33840200+paul-szczepanek-arm@users.noreply.github.com>
2018-11-20 14:42:59 +0000

    beacon updated

--- BLE_Beacon/source/main.cpp
+++ BLE_Beacon/source/main.cpp
@@ -19,20 +19,9 @@
 #include "ble/BLE.h"
 #include "ble/services/iBeacon.h"
 
-static iBeacon* ibeaconPtr;
+static events::EventQueue eventQueue(/* event count */ 16 * EVENTS_EVENT_SIZE);
 
-static EventQueue eventQueue(/* event count */ 4 * EVENTS_EVENT_SIZE);
-
-/**
- * This function is called when the ble initialization process has failled
- */
-void onBleInitError(BLE &ble, ble_error_t error)
-{
-    /* Initialization error handling should go here */
-}
-
-void printMacAddress()
-{
+void printMacAddress() {
     /* Print out device MAC address to the console*/
     Gap::AddressType_t addr_type;
     Gap::Address_t address;
@@ -44,45 +33,72 @@ void printMacAddress()
     printf("%02x\r\n", address[0]);
 }
 
-/**
- * Callback triggered when the ble initialization process has finished
- */
-void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
-{
-    BLE&        ble   = params->ble;
-    ble_error_t error = params->error;
+class BeaconDemo : ble::Gap::EventHandler {
+public:
+    BeaconDemo(BLE &ble, events::EventQueue &eventQueue) :
+        _ble(ble),
+        _eventQueue(eventQueue) { }
 
-    if (error != BLE_ERROR_NONE) {
-        /* In case of error, forward the error handling to onBleInitError */
-        onBleInitError(ble, error);
-        return;
+    void start() {
+        _ble.gap().setEventHandler(this);
+
+        _ble.init(this, &BeaconDemo::initComplete);
+
+        _eventQueue.dispatch_forever();
     }
 
-    /* Ensure that it is the default instance of BLE */
-    if(ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
-        return;
+private:
+    /** Callback triggered when the ble initialization process has finished */
+    void initComplete(BLE::InitializationCompleteCallbackContext *params) {
+        if (params->error != BLE_ERROR_NONE) {
+            printf("Ble initialization failed.");
+            return;
+        }
+
+        printMacAddress();
+
+        /**
+         * The Beacon payload has the following composition:
+         * 128-Bit / 16byte UUID = E2 0A 39 F4 73 F5 4B C4 A1 2F 17 D1 AD 07 A9 61
+         * Major/Minor  = 0x1122 / 0x3344
+         * Tx Power     = 0xC8 = 200, 2's compliment is 256-200 = (-56dB)
+         *
+         * Note: please remember to calibrate your beacons TX Power for more accurate results.
+         */
+        static const uint8_t uuid[] = { 0xE2, 0x0A, 0x39, 0xF4, 0x73, 0xF5, 0x4B, 0xC4,
+                                        0xA1, 0x2F, 0x17, 0xD1, 0xAD, 0x07, 0xA9, 0x61 };
+        uint16_t majorNumber = 1122;
+        uint16_t minorNumber = 3344;
+        uint16_t txPower     = 0xC8;
+
+        _ibeaconPtr = new iBeacon(_ble, uuid, majorNumber, minorNumber, txPower);
+
+        ble::AdvertisingParameters adv_parameters(
+            ble::advertising_type_t::ADV_CONNECTABLE_UNDIRECTED,
+            ble::adv_interval_t(ble::millisecond_t(1000))
+        );
+
+        _ble.gap().setAdvertisingParameters(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            adv_parameters
+        );
+
+        _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
     }
 
-    /**
-     * The Beacon payload has the following composition:
-     * 128-Bit / 16byte UUID = E2 0A 39 F4 73 F5 4B C4 A1 2F 17 D1 AD 07 A9 61
-     * Major/Minor  = 0x1122 / 0x3344
-     * Tx Power     = 0xC8 = 200, 2's compliment is 256-200 = (-56dB)
-     *
-     * Note: please remember to calibrate your beacons TX Power for more accurate results.
-     */
-    static const uint8_t uuid[] = {0xE2, 0x0A, 0x39, 0xF4, 0x73, 0xF5, 0x4B, 0xC4,
-                                   0xA1, 0x2F, 0x17, 0xD1, 0xAD, 0x07, 0xA9, 0x61};
-    uint16_t majorNumber = 1122;
-    uint16_t minorNumber = 3344;
-    uint16_t txPower     = 0xC8;
-    ibeaconPtr = new iBeacon(ble, uuid, majorNumber, minorNumber, txPower);
+private:
+    /* Event handler */
 
-    ble.gap().setAdvertisingInterval(1000); /* 1000ms. */
-    ble.gap().startAdvertising();
+    void onDisconnection(const ble::DisconnectionEvent&) {
+        _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+    }
 
-    printMacAddress();
-}
+private:
+    BLE &_ble;
+    events::EventQueue &_eventQueue;
+
+    iBeacon* _ibeaconPtr;
+};
 
 void scheduleBleEventsProcessing(BLE::OnEventsToProcessCallbackContext* context) {
     BLE &ble = BLE::Instance();
@@ -93,9 +109,9 @@ int main()
 {
     BLE &ble = BLE::Instance();
     ble.onEventsToProcess(scheduleBleEventsProcessing);
-    ble.init(bleInitComplete);
 
-    eventQueue.dispatch_forever();
+    BeaconDemo demo(ble, eventQueue);
+    demo.start();
 
     return 0;
 }

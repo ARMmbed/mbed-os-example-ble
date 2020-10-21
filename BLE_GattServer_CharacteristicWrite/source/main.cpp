3acc13e790a1becf480f00413376411d853e9f4f
Paul Szczeanek <paul.szczepanek@arm.com>
2020-10-21 16:51:51 +0100

    simplify gatt server demo by externalising boiler plate

--- BLE_GattServer_CharacteristicWrite/source/main.cpp
+++ BLE_GattServer_CharacteristicWrite/source/main.cpp
@@ -17,147 +17,77 @@
 #include <events/mbed_events.h>
 #include <mbed.h>
 #include "ble/BLE.h"
-#include "LEDService.h"
-#include "pretty_printer.h"
-
-const static char DEVICE_NAME[] = "LED";
+#include "gatt_server_process.h"
 
 static EventQueue event_queue(/* event count */ 10 * EVENTS_EVENT_SIZE);
 
-class LEDDemo : ble::Gap::EventHandler {
-public:
-    LEDDemo(BLE &ble, events::EventQueue &event_queue) :
-        _ble(ble),
-        _event_queue(event_queue),
-        _alive_led(LED1, 1),
-        _actuated_led(LED2, 0),
-        _led_uuid(LEDService::LED_SERVICE_UUID),
-        _led_service(NULL),
-        _adv_data_builder(_adv_buffer) { }
+class GattServerDemo : ble::Gap::EventHandler {
 
-    ~LEDDemo() {
-        delete _led_service;
+    const static uint16_t EXAMPLE_SERVICE_UUID         = 0xA000;
+    const static uint16_t WRITABLE_CHARACTERISTIC_UUID = 0xA001;
+
+public:
+    GattServerDemo()
+    {
+        const UUID uuid = WRITABLE_CHARACTERISTIC_UUID;
+        _writable_characteristic = new ReadWriteGattCharacteristic<uint8_t> (uuid, &_characteristic_value);
+
+        if (!_writable_characteristic) {
+            printf("Allocation of ReadWriteGattCharacteristic failed\r\n");
+        }
     }
 
-    void start() {
-        _ble.gap().setEventHandler(this);
+    ~GattServerDemo()
+    {
+    }
 
-        _ble.init(this, &LEDDemo::on_init_complete);
+    void start(BLE &ble, events::EventQueue &event_queue)
+    {
+        const UUID uuid = EXAMPLE_SERVICE_UUID;
+        GattCharacteristic* charTable[] = { _writable_characteristic };
+        GattService example_service(uuid, charTable, 1);
 
-        _event_queue.call_every(500, this, &LEDDemo::blink);
+        ble.gattServer().addService(example_service);
 
-        _event_queue.dispatch_forever();
+        ble.gattServer().onDataWritten(this, &GattServerDemo::on_data_written);
+
+        printf("\r\nExample service added with UUID 0xA000\r\n");
+        printf("Connect and write to characteristic 0xA001\r\n\r\n");
     }
 
 private:
-    /** Callback triggered when the ble initialization process has finished */
-    void on_init_complete(BLE::InitializationCompleteCallbackContext *params) {
-        if (params->error != BLE_ERROR_NONE) {
-            printf("Ble initialization failed.");
-            return;
-        }
-
-        _led_service = new LEDService(_ble, false);
-
-        _ble.gattServer().onDataWritten(this, &LEDDemo::on_data_written);
-
-        print_mac_address();
-
-        start_advertising();
-    }
-
-    void start_advertising() {
-        /* Create advertising parameters and payload */
-
-        ble::AdvertisingParameters adv_parameters(
-            ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
-            ble::adv_interval_t(ble::millisecond_t(1000))
-        );
-
-        _adv_data_builder.setFlags();
-        _adv_data_builder.setLocalServiceList(mbed::make_Span(&_led_uuid, 1));
-        _adv_data_builder.setName(DEVICE_NAME);
-
-        /* Setup advertising */
-
-        ble_error_t error = _ble.gap().setAdvertisingParameters(
-            ble::LEGACY_ADVERTISING_HANDLE,
-            adv_parameters
-        );
-
-        if (error) {
-            printf("_ble.gap().setAdvertisingParameters() failed\r\n");
-            return;
-        }
-
-        error = _ble.gap().setAdvertisingPayload(
-            ble::LEGACY_ADVERTISING_HANDLE,
-            _adv_data_builder.getAdvertisingData()
-        );
-
-        if (error) {
-            printf("_ble.gap().setAdvertisingPayload() failed\r\n");
-            return;
-        }
-
-        /* Start advertising */
-
-        error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
-
-        if (error) {
-            printf("_ble.gap().startAdvertising() failed\r\n");
-            return;
-        }
-    }
-
     /**
      * This callback allows the LEDService to receive updates to the ledState Characteristic.
      *
      * @param[in] params Information about the characterisitc being updated.
      */
-    void on_data_written(const GattWriteCallbackParams *params) {
-        if ((params->handle == _led_service->getValueHandle()) && (params->len == 1)) {
-            _actuated_led = *(params->data);
+    void on_data_written(const GattWriteCallbackParams *params)
+    {
+        if ((params->handle == _writable_characteristic->getValueHandle()) && (params->len == 1)) {
+            printf("New characteristic value written: %x\r\n", *(params->data));
         }
     }
 
-    void blink() {
-        _alive_led = !_alive_led;
-    }
-
 private:
-    /* Event handler */
-
-    void onDisconnectionComplete(const ble::DisconnectionCompleteEvent&) {
-        _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
-    }
-
-private:
-    BLE &_ble;
-    events::EventQueue &_event_queue;
-    DigitalOut _alive_led;
-    DigitalOut _actuated_led;
-
-    UUID _led_uuid;
-    LEDService *_led_service;
-
-    uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
-    ble::AdvertisingDataBuilder _adv_data_builder;
+    ReadWriteGattCharacteristic<uint8_t> *_writable_characteristic = nullptr;
+    uint8_t _characteristic_value = 0;
 };
-
-/** Schedule processing of events from the BLE middleware in the event queue. */
-void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context) {
-    event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
-}
 
 int main()
 {
     BLE &ble = BLE::Instance();
-    ble.onEventsToProcess(schedule_ble_events);
 
-    LEDDemo demo(ble, event_queue);
-    demo.start();
+    printf("GattServer demo of writable characteristic\r\n");
+
+    GattServerDemo demo;
+
+    /* this process will handle basic setup and advertising for us */
+    BLEProcess ble_process(event_queue, ble);
+
+    /* once it's done it will let us continue with our demo*/
+    ble_process.on_init(callback(&demo, &GattServerDemo::start));
+
+    ble_process.start();
 
     return 0;
 }
-

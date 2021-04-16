a6792ff9fc1f4e0b97df6cb3ca3bf8d553c89234
Abbas Bracken Ziad <abbas.brackenziad@arm.com>
2021-04-16 21:48:14 +0100

    Mock: add FOTA target

---
+++ BLE_GattServer_FOTAService/source/BlockDeviceFOTAEventHandler.cpp
@@ -0,0 +1,129 @@
+/*
+ * Mbed-OS Microcontroller Library
+ * Copyright (c) 2020-2021 Embedded Planet
+ * Copyright (c) 2020-2021 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+#include "BlockDeviceFOTAEventHandler.h"
+
+#include "BlockDevice.h"
+#include "mbed-trace/mbed_trace.h"
+
+#define TRACE_GROUP "FOTA"
+
+BlockDeviceFOTAEventHandler::BlockDeviceFOTAEventHandler(mbed::BlockDevice &bd, events::EventQueue &eq) :
+                                                         _bd(bd), _eq(eq)
+{
+}
+
+BlockDeviceFOTAEventHandler::~BlockDeviceFOTAEventHandler()
+{
+    delete _bd_eraser;
+}
+
+FOTAService::StatusCode_t BlockDeviceFOTAEventHandler::on_binary_stream_written(
+        FOTAService &fota_service,
+        mbed::Span<const uint8_t> buffer)
+{
+    tr_info("BSC written: programming %" PRIu64 "bytes at address %" PRIu64 "",
+            static_cast<uint64_t>(buffer.size()),
+            _addr);
+
+    int error = _bd.program(buffer.data(), _addr, buffer.size());
+    if (error) {
+        tr_error("Programming block device failed: 0x%x", error);
+        return FOTAService::FOTA_STATUS_MEMORY_ERROR;
+    }
+
+    _addr += buffer.size();
+
+    return FOTAService::FOTA_STATUS_OK;
+}
+
+GattAuthCallbackReply_t BlockDeviceFOTAEventHandler::on_control_written(
+        FOTAService &fota_service,
+        mbed::Span<const uint8_t> buffer)
+{
+    _fota_service = &fota_service;
+
+    switch (buffer[0]) {
+        case FOTAService::FOTA_NO_OP:
+        {
+            break;
+        }
+
+        case FOTAService::FOTA_START:
+        {
+            uint8_t status = FOTAService::FOTA_STATUS_OK;
+
+            tr_info("Starting FOTA session\r\n");
+            fota_service.start_fota_session();
+
+            /* We must erase the update block device before accepting BSC writes
+             * Therefore, set the status characteristic to XOFF to initiate a "delayed start"
+             */
+            fota_service.set_xoff();
+
+            /* Delete previously allocated eraser */
+            delete _bd_eraser;
+
+            /* Erase update block device */
+            _bd_eraser = new PeriodicBlockDeviceEraser(_bd, _eq);
+            tr_info("Erasing block device: size=%" PRIu64 "",
+                    static_cast<uint64_t>(_bd.size()));
+            bd_size_t erase_size = _bd.get_erase_size();
+            int error = _bd_eraser->start_erase(0, _bd.size(),
+                   mbed::callback(this, &BlockDeviceFOTAEventHandler::on_bd_erased));
+
+            MBED_ASSERT(error);
+
+            fota_service.notify_status(status);
+            break;
+        }
+
+        case FOTAService::FOTA_STOP:
+        {
+            tr_info("Stopping FOTA session\r\n");
+            fota_service.stop_fota_session();
+            break;
+        }
+
+        case FOTAService::FOTA_COMMIT:
+        {
+            /* Application-specific */
+            break;
+        }
+
+        default:
+        {
+            return static_cast<GattAuthCallbackReply_t>
+            (FOTAService::AUTH_CALLBACK_REPLY_ATTERR_UNSUPPORTED_OPCODE);
+        }
+    }
+
+    return AUTH_CALLBACK_REPLY_SUCCESS;
+}
+
+void BlockDeviceFOTAEventHandler::on_bd_erased(int result)
+{
+    if (result != mbed::BD_ERROR_OK) {
+        tr_error("Failed to erase block device: notifying client");
+        _fota_service->notify_status(FOTAService::FOTA_STATUS_MEMORY_ERROR);
+    } else {
+        tr_info("Successfully erased the update block Device");
+        _fota_service->set_xon();
+    }
+}
---
+++ BLE_GattServer_FOTAService/source/main.cpp
@@ -0,0 +1,204 @@
+/*
+ * Mbed-OS Microcontroller Library
+ * Copyright (c) 2020-2021 Embedded Planet
+ * Copyright (c) 2020-2021 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+#include "ble/BLE.h"
+#include "ble/gap/Gap.h"
+
+#include "ble-service-fota/FOTAService.h"
+
+#include "blockdevice/SlicingBlockDevice.h"
+#include "BlockDeviceFOTAEventHandler.h"
+
+#include "mbed-trace/mbed_trace.h"
+
+#define TRACE_GROUP "MAIN"
+
+const static char DEVICE_NAME[] = "FOTA";
+
+static events::EventQueue event_queue(/* event count */ 10 * EVENTS_EVENT_SIZE);
+static ChainableGapEventHandler chainable_gap_event_handler;
+static ChainableGattServerEventHandler chainable_gatt_server_event_handler;
+
+mbed::BlockDevice* get_secondary_bd()
+{
+    mbed::BlockDevice* default_bd = mbed::BlockDevice::get_default_instance();
+    static mbed::SlicingBlockDevice sliced_bd(default_bd, 0x00000, 0xC0000);
+    return &sliced_bd;
+}
+
+class FOTADemoEventHandler : public BlockDeviceFOTAEventHandler {
+public:
+    FOTADemoEventHandler(mbed::BlockDevice &bd, events::EventQueue &eq) :
+            BlockDeviceFOTAEventHandler(bd, eq)
+    {
+    }
+};
+
+class FOTAServiceDemo : public ble::Gap::EventHandler,
+                        public FOTAService::EventHandler {
+public:
+    FOTAServiceDemo(BLE &ble,
+                    events::EventQueue &eq,
+                    ChainableGapEventHandler &chainable_gap_eh,
+                    ChainableGattServerEventHandler &chainable_gatt_server_eh) :
+                    _ble(ble),
+                    _event_queue(eq),
+                    _chainable_gap_event_handler(chainable_gap_eh),
+                    _chainable_gatt_server_event_handler(chainable_gatt_server_eh),
+                    _fota_handler(*get_secondary_bd(), eq),
+                    _fota_service(_ble,
+                                  _event_queue,
+                                  _chainable_gap_event_handler,
+                                  _chainable_gatt_server_event_handler,
+                                  "1.0.0",
+                                  "1.0.0",
+                                  "Demo"),
+                    _adv_data_builder(_adv_buffer)
+    {
+    }
+
+    void start()
+    {
+        _ble.init(this, &FOTAServiceDemo::on_init_complete);
+
+        _event_queue.dispatch_forever();
+    }
+
+    void disconnect(ble::local_disconnection_reason_t disconnection_reason)
+    {
+        _ble.gap().disconnect(_connection_handle, disconnection_reason);
+    }
+
+private:
+    void on_init_complete(BLE::InitializationCompleteCallbackContext *params)
+    {
+        if (params->error != BLE_ERROR_NONE) {
+            tr_error("BLE initialization failed.");
+            return;
+        }
+
+        _chainable_gap_event_handler.addEventHandler(this);
+        _ble.gap().setEventHandler(&_chainable_gap_event_handler);
+
+        _ble.gattServer().setEventHandler(&_chainable_gatt_server_event_handler);
+
+        _fota_service.init();
+
+        _fota_service.set_event_handler(&_fota_handler);
+
+        start_advertising();
+    }
+
+    void start_advertising()
+    {
+        ble::AdvertisingParameters adv_parameters(
+                ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
+                ble::adv_interval_t(ble::millisecond_t(100))
+        );
+
+        _adv_data_builder.setFlags();
+        _adv_data_builder.setAppearance(ble::adv_data_appearance_t::UNKNOWN);
+        _adv_data_builder.setName(DEVICE_NAME);
+
+        ble_error_t error = _ble.gap().setAdvertisingParameters(
+                ble::LEGACY_ADVERTISING_HANDLE,
+                adv_parameters
+        );
+
+        if (error) {
+            tr_error("_ble.gap().setAdvertisingParameters() failed\r\n");
+            return;
+        }
+
+        error = _ble.gap().setAdvertisingPayload(
+                ble::LEGACY_ADVERTISING_HANDLE,
+                _adv_data_builder.getAdvertisingData()
+        );
+
+        if (error) {
+            tr_error("_ble.gap().setAdvertisingPayload() failed\r\n");
+            return;
+        }
+
+        error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            tr_error("_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+
+        tr_info("Device advertising, please connect\r\n");
+    }
+
+private:
+    void onConnectionComplete(const ble::ConnectionCompleteEvent &event) override
+    {
+        if (event.getStatus() == ble_error_t::BLE_ERROR_NONE) {
+            _connection_handle = event.getConnectionHandle();
+            tr_info("Client connected, you may now subscribe to updates\r\n");
+        }
+    }
+
+    void onDisconnectionComplete(const ble::DisconnectionCompleteEvent &event) override
+    {
+        printf("Client disconnected, restarting advertising\r\n");
+
+        ble_error_t error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            tr_error("_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+    }
+
+private:
+    BLE &_ble;
+    events::EventQueue &_event_queue;
+    ChainableGapEventHandler &_chainable_gap_event_handler;
+    ChainableGattServerEventHandler &_chainable_gatt_server_event_handler;
+
+    FOTADemoEventHandler _fota_handler;
+    FOTAService _fota_service;
+
+    uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE]{};
+    ble::AdvertisingDataBuilder _adv_data_builder;
+
+    ble::connection_handle_t _connection_handle = 0;
+};
+
+void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context)
+{
+    event_queue.call(mbed::Callback<void()>(&context->ble, &BLE::processEvents));
+}
+
+int main()
+{
+    mbed_trace_init();
+
+    BLE &ble = BLE::Instance();
+    ble.onEventsToProcess(schedule_ble_events);
+
+    FOTAServiceDemo demo(ble,
+                         event_queue,
+                         chainable_gap_event_handler,
+                         chainable_gatt_server_event_handler);
+    demo.start();
+
+    return 0;
+}

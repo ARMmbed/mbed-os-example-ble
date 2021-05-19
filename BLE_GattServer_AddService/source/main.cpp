06082e282a2590d145c7038fc4f889f7d0e00729
Paul Szczepanek <paul.szczepanek@arm.com>
2021-05-19 13:40:46 +0100

    fix heartrate value size

--- BLE_GattServer_AddService/source/main.cpp
+++ BLE_GattServer_AddService/source/main.cpp
@@ -124,9 +124,9 @@ private:
         /* you can read in the real value but here we just simulate a value */
         _heartrate_value++;
 
-        /*  100 <= bpm value <= 175 */
-        if (_heartrate_value == 175) {
-            _heartrate_value = 100;
+        /*  60 <= bpm value < 110 */
+        if (_heartrate_value == 110) {
+            _heartrate_value = 60;
         }
 
         _heartrate_service.updateHeartRate(_heartrate_value);
@@ -160,7 +160,7 @@ private:
     events::EventQueue &_event_queue;
 
     UUID _heartrate_uuid;
-    uint8_t _heartrate_value;
+    uint16_t _heartrate_value;
     HeartRateService _heartrate_service;
 
     uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
